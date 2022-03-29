5388d8dacb06a7eca09bdffd445e72fae3541616
kx78 <98497308+kx78@users.noreply.github.com>
2022-03-29 15:31:45 -0400

    Update RC_Driven_Wheels.ino

--- RC_Driven_Wheels.ino
+++ RC_Driven_Wheels.ino
@@ -2,7 +2,7 @@
 SoftwareSerial Bluetooth(10, 9); // RX, TX
 
 // Output pins for the motors
-int IN1 =5;
+int IN1 = 5;
 int IN2 = 6;
 int IN3 = 4;
 int IN4 = 3;
@@ -18,6 +18,11 @@ int BASE1 = 14;
 int BASE2 = 15;
 int EN_BASE = 16;
 
+// output pin for the base motor
+int TOP1 = 17;
+int TOP2 = 18;
+int EN_TOP = 19;
+
 // Received data via bluetooth
 char data;
 char check_data;
@@ -25,7 +30,7 @@ int stop_pressed = 0;
 int btVal;
 
 void setup() {
- // Declare output?
+  // Declare output?
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
@@ -36,7 +41,10 @@ void setup() {
   pinMode(BASE1, OUTPUT);
   pinMode(BASE2, OUTPUT);
   pinMode(EN_BASE, OUTPUT);
-// Initial output value (during initialization?)
+  pinMode(TOP1, OUTPUT);
+  pinMode(TOP2, OUTPUT);
+  pinMode(EN_TOP, OUTPUT);
+  // Initial output value (during initialization?)
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
@@ -44,104 +52,123 @@ void setup() {
   digitalWrite(PUMP1, LOW);
   digitalWrite(PUMP2, LOW);
   digitalWrite(EN_PUMP, LOW);
-    digitalWrite(BASE1, LOW);
+  digitalWrite(BASE1, LOW);
   digitalWrite(BASE2, LOW);
   digitalWrite(EN_BASE, LOW);
-//Set the baud rate to your Bluetooth module.
+  digitalWrite(TOP1, LOW);
+  digitalWrite(TOP2, LOW);
+  digitalWrite(EN_TOP, LOW);
+  //Set the baud rate to your Bluetooth module.
   Bluetooth.begin(9600);
-  Serial.begin(9600); 
-  Serial.println("Waiting for command..."); 
+  Serial.begin(9600);
+  Serial.println("Waiting for command...");
 }
 
 void loop()
 {
- while (Bluetooth.available())
- {  
-  {  
+  while (Bluetooth.available())
+  {
+    {
       data = Bluetooth.read();
-      Serial.print(data);             
-  } 
-    
-    //btVal = (data.toInt());
-    //Serial.print("BlueTooth Value ");
-    //Serial.println(btVal);    
+      Serial.print(data);
+    }
 
-
-
-  switch (data) 
-   {
-      case 'F':                                
-        //Serial.println("Forward");
+    switch (data)
+    {
+      case 'F':
         forward();
-        Bluetooth.println("On!");
         break;
 
-      case 'B':                 
-       //Serial.println("Reverse");
+      case 'B':
         reverse();
         break;
 
-      case 'L':         
-       //Serial.println("Left");
+      case 'L':
         left();
         break;
-        
-      case 'R':                     
-        //Serial.println("Right");
+
+      case 'R':
         right();
         break;
-        
-      case 'S':                                            
-        //Serial.println("Stop");
-        stoprobot();
-        break;  
 
-      case 'W':                                            
-        //Serial.println("Stop");
+      case 'S':
+        stoprobot();
+        break;
+
+      case 'P':
         pumpON();
         break;
-      
-      case 'w':                                            
-        //Serial.println("Stop");
+
+      case 'p':
         pumpOFF();
         break;
 
-      case 'U':                                            
-        //Serial.println("Stop");
-        BaseON();
+      case 'M':
+        Base_Forward();
         break;
-      
-      case 'u':                                            
-        //Serial.println("Stop");
-        BaseOFF();
+
+      case 'm':
+        Base_Reverse();
         break;
-    
+
+      case 'N':
+        Base_Stop();
+        break;
+
+      case 'T':
+        Top_Forward();
+        break;
+
+      case 't':
+        Top_Reverse();
+        break;
+
+      case 'Q':
+        Top_Stop();
+        break;
+
+      case 'Z':
+        stoprobot();
+        Base_Stop();
+        Top_Stop();
+        pumpOFF();
+        break;
+
       case 'A':  // Put button character for path A in '' that corresponds to app, press button once (not hold) like the pump
         // Actual path needs calibration
-        
+
         // Plant 1
         left();
         delay(2000); //2000ms=2s time delay holds left function on for 2s (turn left for 2s)
         forward();
         delay(5000); //5s forward
-        reverse_left();
+        right();
         delay(2000);
         forward();
         delay(1000);
         stoprobot();
         Serial.println("Moved to Plant 1");
-        // Put code for arm movement and activate pump
-        BaseON();
-        delay(3000);
-        BaseOFF();
+        // Arm movement and pump actuation
+        Base_Forward();
+        delay(2000);
+        Base_Stop();
+        Top_Forward();
+        delay(2000);
+        Top_Stop();
         pumpON();
-        delay(3000);
+        delay(5000);
         pumpOFF();
         Serial.println("Plant 1 Watered");
 
         // Check if the stop button is pressed during previous actions
         // is there a better way? Buttons pushed while running delays might result in buffer overflow
-        check_stop();
+        while (Bluetooth.available()) {
+          check_data = Bluetooth.read();
+          if (check_data == 'Z') {  // Stop button pressed
+            stop_pressed = 1;
+            break;
+          }
+        }
         if (stop_pressed == 1) {
           Serial.println("Stopped, Waiting for instruction...");
           stop_pressed = 0;
@@ -152,8 +179,8 @@ void loop()
 
         // Plant 2
         reverse();
-        delay(1000);
-        reverse_left();
+        delay(3000);
+        right();
         delay(3000);
         forward();
         delay(2000);
@@ -163,17 +190,26 @@ void loop()
         delay(1000);
         stoprobot();
         Serial.println("Moved to Plant 2");
-        // Put code for arm movement and activate pump
-        BaseON();
-        delay(3000);
-        BaseOFF();
+        // Arm movement and pump actuation
+        Base_Reverse();
+        delay(2000);
+        Base_Stop();
+        Top_Reverse();
+        delay(2000);
+        Top_Stop();
         pumpON();
-        delay(3000);
+        delay(5000);
         pumpOFF();
         Serial.println("Plant 2 Watered");
 
         // Check again if stop button is pressed...
-        check_stop();
+        while (Bluetooth.available()) {
+          check_data = Bluetooth.read();
+          if (check_data == 'Z') {  // Stop button pressed
+            stop_pressed = 1;
+            break;
+          }
+        }
         if (stop_pressed == 1) {
           Serial.println("Stopped, Waiting for instruction...");
           stop_pressed = 0;
@@ -184,19 +220,20 @@ void loop()
 
         Serial.println("Path A Done");
         Serial.println("Waiting for instruction...");
+
         break;
 
 
+    }
+
   }
 
- } 
- 
-                                                              
-   if (Bluetooth.available() < 0)                              
-    {
-     //Serial.println("No Bluetooth Data ");          
-    }
-  
+
+  if (Bluetooth.available() < 0)
+  {
+    //Serial.println("No Bluetooth Data ");
+  }
+
 }
 
 void forward()
@@ -253,14 +290,21 @@ void pumpOFF()
   digitalWrite(EN_PUMP, LOW);
 }
 
-void BaseON()
+void Base_Forward()
 {
-  digitalWrite(PUMP1, LOW);
-  digitalWrite(PUMP2, HIGH);
+  digitalWrite(BASE1, HIGH);
+  digitalWrite(BASE2, LOW);
   digitalWrite(EN_BASE, HIGH);
 }
 
-void BaseOFF()
+void Base_Reverse()
+{
+  digitalWrite(BASE1, LOW);
+  digitalWrite(BASE2, HIGH);
+  digitalWrite(EN_BASE, HIGH);
+}
+
+void Base_Stop()
 {
   digitalWrite(BASE1, LOW);
   digitalWrite(BASE2, LOW);
@@ -268,29 +312,23 @@ void BaseOFF()
 }
 
 
-void reverse_left()
+void Top_Forward()
 {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
+  digitalWrite(TOP1, HIGH);
+  digitalWrite(TOP2, LOW);
+  digitalWrite(EN_TOP, HIGH);
 }
 
-void reverse_right()
+void Top_Reverse()
 {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
+  digitalWrite(TOP1, LOW);
+  digitalWrite(TOP2, HIGH);
+  digitalWrite(EN_TOP, HIGH);
 }
 
-void check_stop()
+void Top_Stop()
 {
-  while (Bluetooth.available()) {
-    check_data = Bluetooth.read();
-    if (check_data == 'S') {  // Stop button pressed
-      stop_pressed = 1;
-      break;
-    }
-  }
+  digitalWrite(TOP1, LOW);
+  digitalWrite(TOP2, LOW);
+  digitalWrite(EN_TOP, LOW);
 }
